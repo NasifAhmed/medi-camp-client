4f7e2f04c3c0d50b07441931a0f9efb00265e927
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-25 22:55:20 +0600

    Extract drop down and make routes dynamic

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,202 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { ReactNode, useContext } from "react";
+// import { AuthContext } from "../providers/AuthProvider";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { ThemeButton } from "./ThemeButton";
+import { NavBarDropDown } from "./NavBarDropDown";
+// import { useAxios } from "../hooks/useAxios";
+
+type route = {
+    name: string;
+    route: string;
+};
+
+const NavBar = () => {
+    // [dropDownState, setDropDownState] = useState([]);
+
+    // const { user, logOut, setTokenState, tokenState } = useContext(AuthContext);
+    // const axios = useAxios(true);
+    const navigate = useNavigate();
+    // const handleLogOut = () => {
+    //     const userEmail = user.email;
+    //     logOut()
+    //         .then((res) => {
+    //             if (tokenState) {
+    //                 axios
+    //                     .post(
+    //                         "/delete-token",
+    //                         { email: userEmail },
+    //                         { withCredentials: true }
+    //                     )
+    //                     .then((res) => {
+    //                         console.log(
+    //                             `Token delete response ${JSON.stringify(res)}`
+    //                         );
+    //                         setTokenState(false);
+    //                     });
+    //             }
+    //             console.log(res);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // };
+
+    const routes: route[] = [
+        {
+            name: "Home",
+            route: "/",
+        },
+        {
+            name: "Available Camps",
+            route: "/available-camps",
+        },
+        {
+            name: "About",
+            route: "/about",
+        },
+    ];
+
+    return (
+        <nav className="flex justify-between items-center mb-20">
+            <div
+                className="flex-1 mr-auto flex-col gap-2"
+                onClick={() => navigate("/")}
+            >
+                <Avatar className="mr-2">
+                    <AvatarImage src="https://i.ibb.co/NmRFDbN/logo.png" />
+                </Avatar>
+            </div>
+            <div className="md:flex justify-between items-center gap-10 hidden">
+                {routes.map((data) => (
+                    <>
+                        <NavLink
+                            to={data.route}
+                            className={"transition-colors hover:text-primary"}
+                        >
+                            {data.name}
+                        </NavLink>
+                    </>
+                ))}
+            </div>
+            <div className="flex flex-1 ml-auto justify-end items-center gap-1">
+                {
+                    // user ? (
+                    //     <div className="flex text-xs md:text-base items-center">
+                    //         <Avatar className="mr-2">
+                    //             <AvatarImage src={user.photoURL} />
+                    //             <AvatarFallback>
+                    //                 <img
+                    //                     src="https://i.ibb.co/dWbbNfk/fallback.png"
+                    //                     alt=""
+                    //                 />
+                    //             </AvatarFallback>
+                    //         </Avatar>
+
+                    //         <span className="mr-5">{user.displayName}</span>
+                    //         <Button
+                    //             className="hidden md:block"
+                    //             onClick={handleLogOut}
+                    //         >
+                    //             Log Out
+                    //         </Button>
+                    //     </div>
+                    // ) :
+                    <>
+                        <Button
+                            className="hidden md:block"
+                            onClick={() => navigate("/login")}
+                        >
+                            Log In
+                        </Button>
+                        <Button
+                            className="hidden md:block"
+                            onClick={() => navigate("/register")}
+                        >
+                            Register
+                        </Button>
+                    </>
+                }
+
+                <ThemeButton />
+                <NavBarDropDown routes={routes} />
+                {/* <DropdownMenu>
+                    <DropdownMenuTrigger asChild className="md:hidden">
+                        <Button variant="outline" size="icon">
+                            <Menu />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => navigate("/")}>
+                            Home
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => navigate("/blogs")}>
+                            Blogs
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => navigate("/blogs")}>
+                            Feature Blogs
+                        </DropdownMenuItem>
+
+                        {user && (
+                            <>
+                                <DropdownMenuItem>
+                                    <Button
+                                        variant="ghost"
+                                        onClick={() => navigate("/wishlist")}
+                                    >
+                                        Wishlist
+                                    </Button>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Button
+                                        variant="ghost"
+                                        onClick={() => navigate("/add-blog")}
+                                    >
+                                        Add Blog
+                                    </Button>
+                                </DropdownMenuItem>
+                            </>
+                        )}
+                        <DropdownMenuItem>
+                            {user ? (
+                                <>
+                                    <Button
+                                        className=" md:hidden"
+                                        onClick={handleLogOut}
+                                    >
+                                        Log Out
+                                    </Button>
+                                </>
+                            ) : (
+                                <>
+                                    <Button
+                                        className=" md:hidden"
+                                        onClick={() => navigate("/login")}
+                                    >
+                                        Log In
+                                    </Button>
+                                    <Button
+                                        className=" md:hidden"
+                                        onClick={() => navigate("/register")}
+                                    >
+                                        Register
+                                    </Button>
+                                </>
+                            )}
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu> */}
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ src/components/NavBarDropDown.tsx
@@ -0,0 +1,37 @@
+import { ChevronRight, Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useNavigate } from "react-router-dom";
+
+type route = {
+    name: string;
+    route: string;
+};
+
+export function NavBarDropDown({ routes }: { routes: route[] }) {
+    const navigate = useNavigate();
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="md:hidden" asChild>
+                <Button variant="outline" size="icon">
+                    <Menu className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <ChevronRight className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Menu</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                {routes.map((data) => (
+                    <DropdownMenuItem onClick={() => navigate(data.route)}>
+                        {data.name}
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
