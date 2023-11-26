ac72469886915f5da57dbc73e557e254d531fac4
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-26 11:08:30 +0600

    Fix react query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,12 +8,16 @@ import "./index.css";
 import { ThemeProvider } from "./providers/ThemeProvider";
 import Router from "./router/Router";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <HelmetProvider>
             <ThemeProvider defaultTheme="light" storageKey="medicamp-ui-theme">
-                <ReactQueryDevtools />
-                <RouterProvider router={Router} />
+                <QueryClientProvider client={queryClient}>
+                    <ReactQueryDevtools />
+                    <RouterProvider router={Router} />
+                </QueryClientProvider>
             </ThemeProvider>
         </HelmetProvider>
     </React.StrictMode>
