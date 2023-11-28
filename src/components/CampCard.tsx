6aa0b41b813c38db0f72cd6867bc8dff416e1bf1
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-28 10:05:18 +0600

    Extract camp cards to a component

---
+++ src/components/CampCard.tsx
@@ -0,0 +1,91 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Camp } from "@/types/types";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { DateTime } from "luxon";
+
+type prop = {
+    campData: Camp;
+};
+
+function CampCard({ campData }: prop) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(campData._id);
+    }, []);
+
+    return (
+        <Card className="max-w-4xl w-full">
+            <CardHeader>
+                <CardTitle className="text-2xl">{campData.name}</CardTitle>
+                <CardDescription className="text-lg">
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Date & Time :{" "}
+                        </span>
+                        {DateTime.fromISO(campData.date).toHTTP()}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Venue :{" "}
+                        </span>
+                        {campData.venue}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Target Audience :{" "}
+                        </span>
+                        {campData.target_audience}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Fees :{" "}
+                        </span>
+                        ${campData.fees}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Spcialized Service :{" "}
+                        </span>
+                        {campData.special_service}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Healthcare Professional in Attendance :{" "}
+                        </span>
+                        {campData.doctors.length}
+                    </h3>
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className=" max-h-80 mb-6 overflow-hidden">
+                    <img
+                        className="w-full object-cover object-center"
+                        src={campData.img}
+                        alt=""
+                    />
+                </div>
+                <p>{campData.desc}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button
+                    onClick={() => navigate(`/camp-details/${campData._id}`)}
+                >
+                    Details
+                </Button>
+                <Button>Join Camp</Button>
+            </CardFooter>
+        </Card>
+    );
+}
+
+export default CampCard;
