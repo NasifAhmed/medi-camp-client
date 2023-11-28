2b9de6adb289fb3b912c54c4725f160d06e37680
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-28 10:07:05 +0600

    Add imgBb uploader function

---
+++ src/utils/imgBBupload.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export function imgBBupload(img) {
+    const body = new FormData();
+    body.set("key", `${import.meta.env.VITE_IMGBB_API_KEY}`);
+    body.append("image", img);
+
+    return axios({
+        method: "post",
+        url: "https://api.imgbb.com/1/upload",
+        data: body,
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+}
