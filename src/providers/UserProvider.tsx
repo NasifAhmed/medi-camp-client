f1b9ba0744b6948a2a125063876f4968d61c71f2
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-28 21:47:59 +0600

    Setup user provider form user data from DB

---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,59 @@
+import {
+    ReactNode,
+    createContext,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
+import { useAxios } from "@/hooks/useAxios";
+// import { useAxios } from "../hooks/useAxios";
+
+// Types
+import { Organizer, Participant, Doctor } from "@/types/types";
+import { useQuery } from "@tanstack/react-query";
+import { AuthContext } from "./AuthProvider";
+type userType = Organizer | Participant | Doctor | null;
+type userContextValues = {
+    userFromDB: userType;
+    loading: boolean;
+};
+
+// Default values
+const defaultUserState: userContextValues = {
+    userFromDB: null,
+    loading: true,
+};
+
+export const UserContext = createContext(defaultUserState);
+
+const UserProvider = ({ children }: { children: ReactNode }) => {
+    const authContext = useContext(AuthContext);
+    const axios = useAxios();
+    const [loading, setLoading] = useState(true);
+
+    const userDBQuery = useQuery({
+        queryKey: ["userFromDB", authContext.user?.email],
+        queryFn: async () => {
+            const response = await axios.get<userType>(
+                `/user?email=${authContext.user?.email}`
+            );
+            setLoading(false);
+            return response.data;
+        },
+    });
+
+    return (
+        userDBQuery.data && (
+            <UserContext.Provider
+                value={{
+                    userFromDB: userDBQuery.data[0],
+                    loading,
+                }}
+            >
+                {children}
+            </UserContext.Provider>
+        )
+    );
+};
+
+export default UserProvider;
