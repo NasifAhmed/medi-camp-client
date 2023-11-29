56a49ff781c7411a32aee9fb13d97af3454eca61
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-29 13:59:37 +0600

    Add alert dialog for confirmation

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -8,155 +8,253 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageCamps from "./ManageCamps";
 import ManageRegisteredCamps from "./ManageRegisteredCamps";
+import { useContext } from "react";
+import { UserContext } from "@/providers/UserProvider";
+import RegisteredCamps from "./RegisteredCamps";
+import { useQuery } from "@tanstack/react-query";
+import { useAxios } from "@/hooks/useAxios";
+import { RegisteredParticipant } from "@/types/types";
+import { Separator } from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
+type countResult = {
+    count: string;
+};
 
 function DashBoard() {
+    const { userFromDB } = useContext(UserContext);
+    const axios = useAxios();
+    const navigate = useNavigate();
+
+    const campCountQuery = useQuery({
+        queryKey: ["camps", "count"],
+        queryFn: async (): Promise<countResult | null> => {
+            try {
+                const response = await axios.get(`/camp?count=0`);
+                return response.data;
+            } catch (error) {
+                console.log(`Error getting camp count data : ${error}`);
+                return null;
+            }
+        },
+    });
+
+    const doctorCountQuery = useQuery({
+        queryKey: ["user", "doctor", "count"],
+        queryFn: async (): Promise<countResult | null> => {
+            try {
+                const response = await axios.get(`/user?role=doctor&count=0`);
+                return response.data;
+            } catch (error) {
+                console.log(`Error getting doctor count data : ${error}`);
+                return null;
+            }
+        },
+        enabled: !!(userFromDB?.role === "organizer"),
+    });
+
+    const registeredQuery = useQuery({
+        queryKey: ["dashboard", "registeredParticipants", "all"],
+        queryFn: async (): Promise<RegisteredParticipant[] | null> => {
+            try {
+                const response = await axios.get(`/registered`);
+                // const fees = response.data.reduce((accum, current) => {
+                //     if (current.payment_status) {
+                //         return accum + current;
+                //     }
+                // });
+                // return fees;
+                return response.data;
+            } catch (error) {
+                console.log(`Error getting registered data : ${error}`);
+                return null;
+            }
+        },
+
+        enabled: !!(
+            userFromDB?.role === "organizer" || userFromDB?.role === "doctor"
+        ),
+    });
+
+    const registeredParticipantQuery = useQuery({
+        queryKey: ["dashboard", "registeredParticipants", "participant"],
+        queryFn: async (): Promise<RegisteredParticipant[] | null> => {
+            try {
+                const response = await axios.get(
+                    `/registered?email=${userFromDB?.email}`
+                );
+                // const fees = response.data.reduce((accum, current) => {
+                //     if (current.payment_status) {
+                //         return accum + current;
+                //     }
+                // });
+                // return fees;
+                return response.data;
+            } catch (error) {
+                console.log(`Error getting registered data : ${error}`);
+                return null;
+            }
+        },
+        enabled: !!(userFromDB?.role === "participant"),
+    });
+
+    const feeCalculator = (data: RegisteredParticipant[]) => {
+        let total = 0;
+
+        data.forEach((data) => {
+            if (data.payment_status) {
+                total = total + parseInt(data.registered_camp.fees);
+            }
+        });
+        return total;
+    };
+
     return (
-        <div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+        >
             <Tabs defaultValue="overview" className="space-y-4">
                 <TabsList>
                     <TabsTrigger value="overview">Overview</TabsTrigger>
-                    <TabsTrigger value="manage">Manage Camps</TabsTrigger>
-                    <TabsTrigger value="manage_registered">
-                        Manage Registered Camps
-                    </TabsTrigger>
-                    <TabsTrigger value="notifications" disabled>
-                        Notifications
-                    </TabsTrigger>
+                    {userFromDB && userFromDB.role === "organizer" && (
+                        <>
+                            <TabsTrigger value="manage">
+                                Manage Camps
+                            </TabsTrigger>
+                            <TabsTrigger value="manage_registered">
+                                Manage Registered Camps
+                            </TabsTrigger>
+                        </>
+                    )}
+                    {userFromDB && userFromDB.role === "participant" && (
+                        <>
+                            <TabsTrigger value="registered_camps">
+                                Registered Camps
+                            </TabsTrigger>
+                        </>
+                    )}
                 </TabsList>
                 <TabsContent value="overview" className="space-y-4">
                     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                         <Card>
                             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                                 <CardTitle className="text-sm font-medium">
-                                    Total Revenue
+                                    Total Camps
                                 </CardTitle>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    className="h-4 w-4 text-muted-foreground"
-                                >
-                                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                                </svg>
                             </CardHeader>
                             <CardContent>
                                 <div className="text-2xl font-bold">
-                                    $45,231.89
+                                    {campCountQuery.data?.count}
                                 </div>
-                                <p className="text-xs text-muted-foreground">
-                                    +20.1% from last month
-                                </p>
                             </CardContent>
                         </Card>
-                        <Card>
-                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                <CardTitle className="text-sm font-medium">
-                                    Subscriptions
-                                </CardTitle>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    className="h-4 w-4 text-muted-foreground"
-                                >
-                                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                                    <circle cx="9" cy="7" r="4" />
-                                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                                </svg>
-                            </CardHeader>
-                            <CardContent>
-                                <div className="text-2xl font-bold">+2350</div>
-                                <p className="text-xs text-muted-foreground">
-                                    +180.1% from last month
-                                </p>
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                <CardTitle className="text-sm font-medium">
-                                    Sales
-                                </CardTitle>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    className="h-4 w-4 text-muted-foreground"
-                                >
-                                    <rect
-                                        width="20"
-                                        height="14"
-                                        x="2"
-                                        y="5"
-                                        rx="2"
-                                    />
-                                    <path d="M2 10h20" />
-                                </svg>
-                            </CardHeader>
-                            <CardContent>
-                                <div className="text-2xl font-bold">
-                                    +12,234
-                                </div>
-                                <p className="text-xs text-muted-foreground">
-                                    +19% from last month
-                                </p>
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                <CardTitle className="text-sm font-medium">
-                                    Active Now
-                                </CardTitle>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    className="h-4 w-4 text-muted-foreground"
-                                >
-                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                                </svg>
-                            </CardHeader>
-                            <CardContent>
-                                <div className="text-2xl font-bold">+573</div>
-                                <p className="text-xs text-muted-foreground">
-                                    +201 since last hour
-                                </p>
-                            </CardContent>
-                        </Card>
+                        {userFromDB && userFromDB.role === "organizer" && (
+                            <>
+                                <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            Total Fees Collected
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <div className="text-2xl font-bold">
+                                            {registeredQuery.isLoading ? (
+                                                <span>Loading...</span>
+                                            ) : (
+                                                <span>
+                                                    $
+                                                    {registeredQuery.data &&
+                                                        feeCalculator(
+                                                            registeredQuery.data
+                                                        )}
+                                                </span>
+                                            )}
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                                <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            Healthcare Professional Available
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <div className="text-2xl font-bold">
+                                            {doctorCountQuery.data?.count}
+                                        </div>
+                                    </CardContent>
+                                </Card>
+
+                                <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            Total registration
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <div className="text-2xl font-bold">
+                                            {registeredQuery.data?.length}
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </>
+                        )}
+
+                        {userFromDB && userFromDB.role === "participant" && (
+                            <>
+                                <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            Total camps registered
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <div className="text-2xl font-bold">
+                                            {
+                                                registeredParticipantQuery.data
+                                                    ?.length
+                                            }
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </>
+                        )}
                     </div>
-                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                        <Card className="col-span-4">
+                    <div className="">
+                        <Card className="">
                             <CardHeader>
-                                <CardTitle>Overview</CardTitle>
+                                {userFromDB?.role === "organizer" && (
+                                    <CardTitle>Organizer Dashboard</CardTitle>
+                                )}
+                                {userFromDB?.role === "participant" && (
+                                    <CardTitle>Participant Dashboard</CardTitle>
+                                )}
+                                {userFromDB?.role === "doctor" && (
+                                    <CardTitle>Doctor Dashboard</CardTitle>
+                                )}
                             </CardHeader>
-                            <CardContent className="pl-2">
-                                <h1>Overview</h1>
-                            </CardContent>
-                        </Card>
-                        <Card className="col-span-3">
-                            <CardHeader>
-                                <CardTitle>Recent Sales</CardTitle>
-                                <CardDescription>
-                                    You made 265 sales this month.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <h1>Recent Sales</h1>
+                            <CardContent className="text-center text-xl">
+                                <h2>
+                                    <span className="font-bold">Name : </span>
+                                    {userFromDB?.name}
+                                </h2>
+                                <h2>
+                                    <span className="font-bold">Email : </span>
+                                    {userFromDB?.email}
+                                </h2>
+                                <Separator className="my-5" />
+                                <Button
+                                    onClick={() =>
+                                        navigate(`/${userFromDB?.role}-profile`)
+                                    }
+                                >
+                                    Go to profile
+                                </Button>
                             </CardContent>
                         </Card>
                     </div>
@@ -167,8 +265,11 @@ function DashBoard() {
                 <TabsContent value="manage_registered">
                     <ManageRegisteredCamps />
                 </TabsContent>
+                <TabsContent value="registered_camps">
+                    <RegisteredCamps />
+                </TabsContent>
             </Tabs>
-        </div>
+        </motion.div>
     );
 }
 
